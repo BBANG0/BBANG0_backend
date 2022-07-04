0a30997744f5cf9bfc71a09f1ee1f6620ac90a66
WON <wondang99@naver.com>
2022-07-04 16:06:59 +0900

    first commit

---
+++ src/main/java/com/example/bbang0/Bbang0Application.java
@@ -0,0 +1,13 @@
+package com.example.bbang0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bbang0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bbang0Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bbang0/Bbang0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bbang0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bbang0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
