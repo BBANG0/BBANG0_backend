75be3b14d781221e6781094d38cdcef4f23d797d
WON <wondang99@naver.com>
2022-09-16 19:03:57 +0900

    fix : Apply JwtService to BreadController

--- src/main/java/com/example/bbang0/adapter/controller/BreadController.java
+++ src/main/java/com/example/bbang0/adapter/controller/BreadController.java
@@ -8,9 +8,12 @@ import com.example.bbang0.application.service.BreadService;
 import com.example.bbang0.application.service.JwtService;
 import com.example.bbang0.domain.exception.BaseException;
 import com.example.bbang0.domain.exception.BaseResponse;
+import com.example.bbang0.domain.exception.BaseResponseStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.example.bbang0.domain.exception.BaseResponseStatus.INVALID_BAKERY;
 
 @RestController
 @RequestMapping("/breads")
@@ -30,7 +33,8 @@ public class BreadController {
     public BaseResponse<PostBreadResDto> createBread(@RequestBody PostBreadReqDto postBreadReqDto) {
         try{
             // 빵집 유저 맞는지 확인하는 API을 통해 bakeryid를 미리 알아논다.
-            int bakery_id = breadService.findBakeryIdByUserId(postBreadReqDto.getUser_id());
+            String jwt = jwtService.getUserIdx();
+            int bakery_id = breadService.findBakeryIdByUserId(jwt);
 
             PostBreadResDto postBreadResDto = breadService.createBread(bakery_id, postBreadReqDto);
 
@@ -60,6 +64,11 @@ public class BreadController {
     @PatchMapping("/{bread_id}")
     public BaseResponse<String> modifyBread(@PathVariable ("bread_id") int bread_id, @RequestBody PatchBreadReqDto patchBreadReq) {
         try{
+            String jwt = jwtService.getUserIdx();
+            int bakery_id = breadService.findBakeryIdByUserId(jwt);
+            if (bakery_id!= patchBreadReq.getBakery_id()){
+                throw new BaseException(INVALID_BAKERY);
+            }
             breadService.modifyBread(bread_id, patchBreadReq);
             String result = "빵 정보 수정을 완료하였습니다.";
 
@@ -73,8 +82,14 @@ public class BreadController {
     // 빵 개수만 수정하는 기능
     @ResponseBody
     @PatchMapping("/{bread_id}/count")
-    public BaseResponse<String> modifyBreadCount(@PathVariable ("bread_id") int bread_id, @RequestParam int bread_count) {
+    public BaseResponse<String> modifyBreadCount(@PathVariable ("bread_id") int bread_id, @RequestParam int bread_count,PatchBreadReqDto patchBreadReq) {
         try{
+            String jwt = jwtService.getUserIdx();
+            int bakery_id = breadService.findBakeryIdByUserId(jwt);
+            if (bakery_id!= patchBreadReq.getBakery_id()){
+                System.out.println("bakery_id "+Integer.toString(bakery_id)+"bakery_id "+Integer.toString(patchBreadReq.getBakery_id()) );
+                throw new BaseException(INVALID_BAKERY);
+            }
             breadService.modifyBreadCount(bread_id, bread_count);
             String result = "빵 개수 수정을 완료하였습니다.";
 
@@ -90,6 +105,7 @@ public class BreadController {
     @PatchMapping("/{bread_id}/delete")
     public BaseResponse<String> deleteBread(@PathVariable ("bread_id") int bread_id) {
         try{
+
             breadService.deleteBread(bread_id);
             String result = "bread_id : " + bread_id + "인 빵을 삭제하였습니다.";
 
